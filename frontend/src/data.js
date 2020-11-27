a6cfee62f5dbff19fc85ca9b761c1a4e9a50f137
JennaFi <filimonova.evgeniya@gmail.com>
2020-11-28 02:07:08 +0300

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Oscar de la Renta Shirt',
+            category: 'Shirts',
+            image: '/images/shirt.jpg',
+            price: 220,
+            brand: 'Oscar de la Renta',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'haute couture product'
+        },
+        {
+            _id: '2',
+            name: 'Channel Dress',
+            category: 'Dresses',
+            image: '/images/2.jpg',
+            price: 420,
+            brand: 'Channel',
+            rating: 4.8,
+            numReviews: 19,
+            description: 'haute couture product'
+        },
+        {
+            _id: '3',
+            name: 'Chloe Dress',
+            category: 'Dresses',
+            image: '/images/3.jpg',
+            price: 290,
+            brand: 'Chloe',
+            rating: 4.9,
+            numReviews: 17,
+            description: 'haute couture product'
+        },
+        {
+            _id: '4',
+            name: 'Prada Suit',
+            category: 'Suits',
+            image: '/images/suit.jpg',
+            price: 12520,
+            brand: 'Prada',
+            rating: 5,
+            numReviews: 20,
+            description: 'haute couture product'
+        },
+        {
+            _id: '5',
+            name: 'Max Mara Parka',
+            category: 'Parkas',
+            image: '/images/4.jpg',
+            price: 210,
+            brand: 'Max Mara',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'haute couture product'
+        },
+        {
+            _id: '6',
+            name: 'Givenchy',
+            category: 'Skirts',
+            image: '/images/skirt.jpg',
+            price: 520,
+            brand: 'Givenchy',
+            rating: 5,
+            numReviews: 20,
+            description: 'haute couture product'
+        },
+        
+    ]
+}
+
+export default data;
