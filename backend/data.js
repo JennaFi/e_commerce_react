3c7989f206424a695d31c954f193725d1c9b5a1b
JennaFi <filimonova.evgeniya@gmail.com>
2020-12-01 17:01:26 +0300

    backend added

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Oscar de la Renta Shirt',
+            category: 'Shirts',
+            image: '/images/shirt.jpg',
+            price: 220,
+            countInStock: 10,
+            brand: 'Oscar de la Renta',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'haute couture product'
+        },
+        {
+            _id: '2',
+            name: 'Channel Dress',
+            category: 'Dresses',
+            image: '/images/2.jpg',
+            price: 420,
+            countInStock: 20,
+            brand: 'Channel',
+            rating: 4.8,
+            numReviews: 19,
+            description: 'haute couture product'
+        },
+        {
+            _id: '3',
+            name: 'Chloe Dress',
+            category: 'Dresses',
+            image: '/images/3.jpg',
+            price: 290,
+            countInStock: 0,
+            brand: 'Chloe',
+            rating: 4.9,
+            numReviews: 17,
+            description: 'haute couture product'
+        },
+        {
+            _id: '4',
+            name: 'Prada Suit',
+            category: 'Suits',
+            image: '/images/suit.jpg',
+            price: 12520,
+            countInStock: 5,
+            brand: 'Prada',
+            rating: 5,
+            numReviews: 20,
+            description: 'haute couture product'
+        },
+        {
+            _id: '5',
+            name: 'Max Mara Parka',
+            category: 'Parkas',
+            image: '/images/4.jpg',
+            price: 210,
+            countInStock: 15,
+            brand: 'Max Mara',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'haute couture product'
+        },
+        {
+            _id: '6',
+            name: 'Givenchy',
+            category: 'Skirts',
+            image: '/images/skirt.jpg',
+            price: 520,
+            countInStock: 18,
+            brand: 'Givenchy',
+            rating: 5,
+            numReviews: 20,
+            description: 'haute couture product'
+        },
+        
+    ]
+}
+
+export default data;
