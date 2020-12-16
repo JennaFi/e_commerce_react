c7e00a525151b8342d7e7bc9a8d54889310587cc
JennaFi <filimonova.evgeniya@gmail.com>
2020-12-17 00:46:31 +0300

    signing in and register screen added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,40 @@
-import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { cartReducer } from './reducers/cartReducers';
-import { productListReducer,  productDetailsReducer, } from './reducers/productReducers';
-
+import { applyMiddleware, createStore, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { userRegisterReducer, userSigninReducer } from "./reducers/userReducer";
 
 const initialState = {
-    cart: {
-        cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems'))
-        : [],
-    },
+  userSignin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+  },
 };
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 
 const composeEnhanser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    reducer,
-    initialState,
-    composeEnhanser(applyMiddleware(thunk)));
+  reducer,
+  initialState,
+  composeEnhanser(applyMiddleware(thunk))
+);
 
 export default store;
