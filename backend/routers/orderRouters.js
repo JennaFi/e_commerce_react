c39069713bcceaec79554c12854a10209fdb3f48
JennaFi <filimonova.evgeniya@gmail.com>
2020-12-22 00:47:50 +0300

    paypal added

--- backend/routers/orderRouters.js
+++ backend/routers/orderRouters.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line null
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
@@ -27,4 +28,40 @@ orderRouter.post(
     }
   }),
 );
+
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({message: 'Order Not Found'});
+    }
+  }),
+);
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({message: 'Order Paid', order: updateOrder});
+    } else {
+      res.statusCode(404).send({message: 'Order Not Found'});
+    }
+  }),
+);
+
 export default orderRouter;
